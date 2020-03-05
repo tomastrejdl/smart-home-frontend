c5351bc9cf57b77fc8bf0252fd55012cfc52c069
Tomáš Trejdl <tom.trejdl@seznam.cz>
2020-03-05 12:34:08 +0100

    Jest config

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,9 @@
-import { shallowMount } from "@vue/test-utils";
-import HelloWorld from "@/components/HelloWorld.vue";
+import { shallowMount } from '@vue/test-utils'
+import Home from '@/views/Home.vue'
 
-describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
-});
+describe('Home.vue', () => {
+  it('renders', () => {
+    const wrapper = shallowMount(Home)
+    expect(wrapper.contains('div')).toBe(true)
+  })
+})
